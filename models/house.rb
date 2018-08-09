2b6e13f2f1ef89e93e31e577e27623255ffb7bfa
yingying <chen.yingying.01988@gmail.com>
2018-08-09 15:19:10 +0100

    test pass house logo

--- models/house.rb
+++ models/house.rb
@@ -1,16 +1,17 @@
 require_relative("../db/sqlrunner")
 
 class House
-  attr_reader :name, :id
+  attr_reader :name, :id, :logo
 
   def initialize(options)
     @id = options["id"].to_i()if options["id"]
     @name = options["name"]
+    @logo = options["logo"]
   end
 
   def save()
-    sql = "INSERT INTO houses (name) VALUES ($1) RETURNING id"
-    values = [@name]
+    sql = "INSERT INTO houses (name, logo) VALUES ($1, $2) RETURNING id"
+    values = [@name, @logo]
     result = SqlRunner.run(sql, values).first
     @id = result["id"].to_i
   end
