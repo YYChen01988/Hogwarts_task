cef92d0975d17ba0da439f0b08055de416c86850
yingying <chen.yingying.01988@gmail.com>
2018-08-09 13:01:07 +0100

    add find method for house

--- models/house.rb
+++ models/house.rb
@@ -14,4 +14,18 @@ class House
     result = SqlRunner.run(sql, values).first
     @id = result["id"].to_i
   end
+
+  def self.all()
+    sql = "SELECT * FROM houses"
+    houses = SqlRunner.run(sql)
+    result = houses.map{|house| House.new(house)}
+    return result
+  end
+
+  def self.find_by_id(id)
+    sql = " SELECT * FROM houses WHERE id=$1"
+    values = [id]
+    result = SqlRunner.run(sql, values).first
+    return House.new(result)
+  end
 end
